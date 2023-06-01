d7e1c38d475741eb314a802e2d8376f03bfc494e
onuratakan <atadogan06@gmail.com>
2023-06-01 14:06:51 +0000

    Changed version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(name='keypact',
-version='0.10.0',
+version='0.11.0',
 description="""Efficient Key-Value Data Storage with Multithreaded Simultaneous Writing""",
 long_description="".join(open("README.md", encoding="utf-8").readlines()),
 long_description_content_type='text/markdown',
