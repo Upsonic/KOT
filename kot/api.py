b80c5a8f8a1b346ab3451f0413f4749fcb3b0c38
Onur Atakan ULUSOY <atadogan06@gmail.com>
2023-09-07 00:32:13 +0300

    Fix

--- kot/api.py
+++ kot/api.py
@@ -20,9 +20,6 @@ password = os.environ.get("password","KOT")
 access_key = (os.environ.get("access_key","false").lower() == "true")
 access_key_folder = os.environ.get("access_key_folder","")
 access_key_lists = os.environ.get("access_key_lists","")
-access_key_lists = access_key_lists.split(",")
-if access_key_lists == [""]:
-    access_key_lists = []
 access_key_lists_cache = int(os.environ.get("access_key_lists_cache",0))
 restricted = os.environ.get("restricted","")
 restricted = restricted.split(",")
