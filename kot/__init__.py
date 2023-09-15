e1718caf987bfc27f785591c4e8cc5369010e695
Onur Atakan ULUSOY <atadogan06@gmail.com>
2023-09-16 02:09:32 +0300

    Fix

--- kot/__init__.py
+++ kot/__init__.py
@@ -5,10 +5,10 @@ from .core.kot import HASHES
 from .core.serial import KOT_Serial
 
 from .remote.remote import KOT_Remote
-from .remote.cloud import KOT_Cloud
-from .remote.cloud import KOT_Cloud_Pro
-from .remote.cloud import KOT_Cloud_Dedicated
-from .remote.cloud import KOT_Cloud_Dedicated_Prepare
+from .remote.interface import KOT_Cloud
+from .remote.interface import KOT_Cloud_Pro
+from .remote.interface import KOT_Cloud_Dedicated
+from .remote.interface import KOT_Cloud_Dedicated_Prepare
 from .remote.helper import no_exception
 from .remote.helper import requires
 
