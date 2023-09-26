4f08cd2160d9b51064ab5006d329acbc88b1a871
Onur Atakan ULUSOY <atadogan06@gmail.com>
2023-09-26 09:54:55 +0300

    Update production.py

--- .github/workflows/production.py
+++ .github/workflows/production.py
@@ -10,18 +10,18 @@ webhook = os.environ.get("PRODUCTION_WEBHOOK")
 tag = os.environ.get("PRODUCTION_TAG")
 access_keys = os.environ.get("PRODUCTION_ACCESS_KEYS")
 
-
-response = requests.request("POST", f"{webhook}?SERVICE_TAG={tag}", verify=False)
-
-
-time.sleep(50)
-
-
-server = os.environ.get("PRODUCTION_SERVER")
-password = os.environ.get("PRODUCTION_SERVER_PASS")
-cloud = KOT_Remote("access_key_database", server, password)
-
-cloud.set("access_keys", access_keys, encryption_key=None)
-
-
+try:
+  response = requests.request("POST", f"{webhook}?SERVICE_TAG={tag}", verify=False)
+  
+  
+  time.sleep(50)
+  
+  
+  server = os.environ.get("PRODUCTION_SERVER")
+  password = os.environ.get("PRODUCTION_SERVER_PASS")
+  cloud = KOT_Remote("access_key_database", server, password)
+  
+  cloud.set("access_keys", access_keys, encryption_key=None)
+except:
+  raise Exception("EXCEPTON")
 
